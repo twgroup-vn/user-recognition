6b459c8db2631008510cd079f250dd8b0d3ea99c
AnhThuc <thuclva@twedu.com.vn>
2020-09-21 08:45:54 +0700

    Updates

---
+++ precache-manifest.cd4232ee6409d85bb1353419b6ae15cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9b31a96a0efaa0f1f6579a27356c7c",
+    "url": "/user-recognition/index.html"
+  },
+  {
+    "revision": "530176515baf3a4c1ae0",
+    "url": "/user-recognition/static/css/main.f0479ad2.chunk.css"
+  },
+  {
+    "revision": "85351b31ec5ac4460762",
+    "url": "/user-recognition/static/js/2.ec3fd3fb.chunk.js"
+  },
+  {
+    "revision": "56de15db0528e473e8447ee66bbfb67b",
+    "url": "/user-recognition/static/js/2.ec3fd3fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530176515baf3a4c1ae0",
+    "url": "/user-recognition/static/js/main.fbfa2997.chunk.js"
+  },
+  {
+    "revision": "087aadef8929c4aab4b1",
+    "url": "/user-recognition/static/js/runtime-main.9a3e4f2a.js"
+  },
+  {
+    "revision": "69a2ae22657c512178fff9f503f8fdf7",
+    "url": "/user-recognition/static/media/logo-twg.69a2ae22.svg"
+  }
+]);
