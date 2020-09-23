864aa9f027906a54eafc649c00eba4b7588d2a36
AnhThuc <thuclva@twedu.com.vn>
2020-09-23 09:26:30 +0700

    init data Feed.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,14 +8,15 @@ const homeFeed = [
         badges: ["creativity"],
         carrotsEach: 10,
         createdAt: "2020-09-22T08:17:19.646Z",
-        message: 'Đưa anh lên board',
+        message: 'Hello mọi người',
         type: 'recognition',
         from: {
             profile: {
                 firstName: 'Vo Xuan',
                 lastName: 'Bach',
                 username: 'xuanbach'
-            }
+            },
+            _id: "5f616ea5e53380557cea92a8"
         },
         to: {
             nickname: "",
@@ -26,7 +27,101 @@ const homeFeed = [
                         lastName: 'Thức',
                         username: 'lại văn anhthức'
                     },
-                    isDeleted: false
+                    isDeleted: false,
+                    _id: "5f60a577937a424a11ee2b36"
+                }
+            ]
+        },
+        taggedUsers: []
+    },
+    {
+        _id: 1,
+        badges: ["closer"],
+        carrotsEach: 20,
+        createdAt: "2020-09-23T08:00:00.646Z",
+        message: 'Hello World',
+        type: 'recognition',
+        from: {
+            profile: {
+                firstName: 'Trịnh Xuân',
+                lastName: 'Quyền',
+                username: 'Xuan Quyen'
+            },
+            _id: "5f616ea5e53380557cea92b1"
+        },
+        to: {
+            nickname: "",
+            users: [
+                {
+                    profile: {
+                        firstName: 'Huỳnh Ngọc',
+                        lastName: 'Nga',
+                        username: 'Huỳnh Ngọc Nga'
+                    },
+                    isDeleted: false,
+                    _id: "5f60a577937a424a11ee2b21"
+                }
+            ]
+        },
+        taggedUsers: []
+    },
+    {
+        _id: 2,
+        badges: ["teamwork"],
+        carrotsEach: 50,
+        createdAt: "2020-09-20T08:00:00.646Z",
+        message: 'Good morning',
+        type: 'recognition',
+        from: {
+            profile: {
+                firstName: 'Nguyễn Minh',
+                lastName: 'Tân',
+                username: 'Minh Tân'
+            },
+            _id: "5f616ea5e53380557cea93c2"
+        },
+        to: {
+            nickname: "",
+            users: [
+                {
+                    profile: {
+                        firstName: 'La Văn',
+                        lastName: 'Linh',
+                        username: 'Linh La'
+                    },
+                    isDeleted: false,
+                    _id: "5f60a577937a424a11ee3cb5"
+                }
+            ]
+        },
+        taggedUsers: []
+    },
+    {
+        _id: 3,
+        badges: ["closer"],
+        carrotsEach: 30,
+        createdAt: "2020-09-23T08:00:00.646Z",
+        message: 'Hello World',
+        type: 'recognition',
+        from: {
+            profile: {
+                firstName: 'Quoc Huy',
+                lastName: 'Truong',
+                username: 'Quoc Huy'
+            },
+            _id: "5f616ea5e53380557cea35d2"
+        },
+        to: {
+            nickname: "",
+            users: [
+                {
+                    profile: {
+                        firstName: 'Luan Hoang',
+                        lastName: 'Nguyen',
+                        username: 'Phong Nguyễn'
+                    },
+                    isDeleted: false,
+                    _id: "5f60a577937a424a11ee4xy3"
                 }
             ]
         },
